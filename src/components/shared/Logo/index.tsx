d30e90f6bdb786c3c92ecf1b07a185dbd378e0c0
Rayslla Rodrigues <rayslla.rodrigues.ferreira@gmail.com>
2023-06-03 10:13:36 -0300

    feat: add ajuste ao component logo para exibir em row e column & colorido e brando

--- src/components/shared/Logo/index.tsx
+++ src/components/shared/Logo/index.tsx
@@ -1,16 +1,29 @@
 import React from "react";
 import logo from "../../../assets/images/logo.svg";
+import logoWhite from "../../../assets/images/logo-white.svg";
 import "./style.css";
 
-const Logo: React.FC = () => {
+type TFlexDirection = "row" | "column";
+type TTheme = "colorful" | "white";
+interface ILogo {
+	direction?: TFlexDirection;
+	theme?: TTheme;
+}
+
+const Logo: React.FC<ILogo> = ({ direction = "row", theme = "white" }) => {
 	return (
-		<div className="container-logo">
+		<div
+			className="container-logo"
+			style={{ flexDirection: direction }}>
 			<img
+				style={{ width: direction === "row" ? 40 : "fit-content" }}
 				className="logo-img"
-				src={logo}
+				src={theme === "colorful" ? logo : logoWhite}
 				alt="Hero Hairdresser Logo"
 			/>
-			<span>Hero Hairdresses</span>
+			<span style={{ fontSize: direction === "row" ? "1rem" : "1.5rem" }}>
+				Hero Hairdresses
+			</span>
 		</div>
 	);
 };
