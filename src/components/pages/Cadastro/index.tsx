eb8fdf2addd814c3235455be89069812043a7030
Rayslla Rodrigues <rayslla.rodrigues.ferreira@gmail.com>
2023-06-03 09:43:44 -0300

    feat: add os inputs a tela de cadastro

--- src/components/pages/Cadastro/index.tsx
+++ src/components/pages/Cadastro/index.tsx
@@ -1,7 +1,33 @@
 import React from "react";
 import "./style.css";
+import Logo from "../../shared/Logo";
+import CardComponent from "../../shared/CardComponent";
+import iconPerson from "../../../assets/images/icons/person.svg";
+import iconMail from "../../../assets/images/icons/mail.svg";
+
+import InputIcon from "../../shared/inputIcon";
+import InputIconPassword from "../../shared/inputIconPassword";
 const Cadastro: React.FC = () => {
-	return <div className="cadastro"></div>;
+	return (
+		<div className="cadastro">
+			<div className="container-cadastro">
+				<Logo />
+				<CardComponent>
+					<form action="">
+						<InputIcon
+							placeholder="Nome"
+							startAdornmentImgPath={iconPerson}
+						/>
+						<InputIcon
+							placeholder="Email"
+							startAdornmentImgPath={iconPerson}
+						/>
+						<InputIconPassword />
+					</form>
+				</CardComponent>
+			</div>
+		</div>
+	);
 };
 
 export default Cadastro;
