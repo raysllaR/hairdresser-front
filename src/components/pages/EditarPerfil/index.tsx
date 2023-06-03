32b361106685109d792cfa70c4d5defb70223222
Rayslla Rodrigues <rayslla.rodrigues.ferreira@gmail.com>
2023-06-03 18:41:11 -0300

    feat: add page de editarPerfil

---
+++ src/components/pages/EditarPerfil/index.tsx
@@ -0,0 +1,15 @@
+import Header from "../../shared/Header";
+import React from "react";
+import "./style.css";
+
+const EditarPerfil: React.FC = () => {
+	return (
+		<div
+			id="editar-perfil"
+			className="page">
+			<Header />
+		</div>
+	);
+};
+
+export default EditarPerfil;
