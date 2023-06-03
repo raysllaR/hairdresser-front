dd4ddb7c7d8cc908f707a0692143235014f7fdfd
Rayslla Rodrigues <rayslla.rodrigues.ferreira@gmail.com>
2023-06-03 02:07:53 -0300

    refactor: utilizando o component de FormLogin

--- src/components/pages/Login/index.tsx
+++ src/components/pages/Login/index.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import Card from "@mui/material/Card";
 import "./index.css";
-import { ButtonBase, CardContent, InputBase } from "@mui/material";
+import { CardContent } from "@mui/material";
 import logo from "../../../assets/images/logo.svg";
-import iconMail from "../../../assets/images/icons/mail.svg";
-import iconkey from "../../../assets/images/icons/key.svg";
-import iconVisibility from "../../../assets/images/icons/visibility.svg";
+import FormLogin from "../../shared/formLogin";
 
 const Login: React.FC = () => {
 	return (
@@ -15,54 +13,14 @@ const Login: React.FC = () => {
 					<img
 						className="logo-img"
 						src={logo}
+						alt="Hero Hairdresser Logo"
 					/>
 					<span>Hero Hairdresses</span>
 				</div>
 				<Card className="card">
 					<CardContent className="card-content">
 						<span className="title">Olá! Seja Bem-Vindo!</span>
-						<form
-							className="form"
-							action="">
-							<InputBase
-								id="email-input"
-								className="input"
-								type="email"
-								placeholder="E-mail"
-								startAdornment={
-									<img
-										className="icon"
-										src={iconMail}
-									/>
-								}
-							/>
-
-							<InputBase
-								id="email-input"
-								className="input"
-								placeholder="Senha"
-								type="password"
-								startAdornment={
-									<img
-										className="icon"
-										src={iconkey}
-									/>
-								}
-								endAdornment={
-									<img
-										className="icon"
-										src={iconVisibility}
-									/>
-								}
-							/>
-
-							<ButtonBase
-								className="button"
-								type="submit">
-								Entrar
-							</ButtonBase>
-						</form>
-
+						<FormLogin />
 						<div className="more">
 							<span>
 								Esqueceu sua senha? <a href="">Recuperar</a>
