db1da641bbda5e5be06c593ed03fbc903050b5bb
Rayslla Rodrigues <rayslla.rodrigues.ferreira@gmail.com>
2023-06-03 19:55:44 -0300

    feat: add form de editar usuário na page EditarPerfil

--- src/components/pages/EditarPerfil/index.tsx
+++ src/components/pages/EditarPerfil/index.tsx
@@ -1,6 +1,7 @@
 import Header from "../../shared/Header";
 import React from "react";
 import "./style.css";
+import InputLabel from "../../shared/inputLabel";
 
 const EditarPerfil: React.FC = () => {
 	return (
@@ -8,6 +9,22 @@ const EditarPerfil: React.FC = () => {
 			id="editar-perfil"
 			className="page">
 			<Header />
+			<form action="">
+				<InputLabel label="Nome" />
+				<InputLabel label="E-mail" />
+				<InputLabel
+					label="Senha atual"
+					type="password"
+				/>
+				<InputLabel
+					label="Nova atual"
+					type="password"
+				/>
+				<InputLabel
+					label="Confirmar senha"
+					type="password"
+				/>
+			</form>
 		</div>
 	);
 };
