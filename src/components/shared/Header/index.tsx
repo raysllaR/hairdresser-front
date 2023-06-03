44865ca4d1561b8c174382360d43e68498687654
Rayslla Rodrigues <rayslla.rodrigues.ferreira@gmail.com>
2023-06-03 18:58:00 -0300

    feat: mostrando o dropMenu ao clicar no botão

--- src/components/shared/Header/index.tsx
+++ src/components/shared/Header/index.tsx
@@ -1,18 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../../shared/Logo";
 import iconAcount from "../../../assets/images/icons/account_circle.svg";
-import {
-	ButtonGroup,
-	Button,
-	ClickAwayListener,
-	Paper,
-	MenuList,
-	MenuItem,
-} from "@mui/material";
-
+import { ClickAwayListener, Paper, MenuList, MenuItem } from "@mui/material";
 import "./style.css";
 
 const Header: React.FC = () => {
+	const [showDropMenu, setShowDropMenu]: [
+		boolean,
+		React.Dispatch<React.SetStateAction<boolean>>
+	] = useState(false);
+
+	const toogleShoeDropMenu = () => {
+		setShowDropMenu(!showDropMenu);
+	};
+
 	return (
 		<div className="container-header">
 			<div className="header">
@@ -21,24 +22,28 @@ const Header: React.FC = () => {
 					theme="white"
 				/>
 				<div>
-					<button>
+					<button onClick={toogleShoeDropMenu}>
 						<img
 							src={iconAcount}
 							alt="icon"
 						/>
 						Perfil
 					</button>
-					<Paper className="drop-down">
-						<ClickAwayListener onClickAway={() => console.log("OI")}>
-							<MenuList
-								className="dados-menu"
-								autoFocusItem>
-								<MenuItem>Agendamentos</MenuItem>
-								<MenuItem>Editar perfil</MenuItem>
-								<MenuItem>Sair</MenuItem>
-							</MenuList>
-						</ClickAwayListener>
-					</Paper>
+					{showDropMenu ? (
+						<Paper className="drop-down">
+							<ClickAwayListener onClickAway={toogleShoeDropMenu}>
+								<MenuList
+									className="dados-menu"
+									autoFocusItem>
+									<MenuItem>Agendamentos</MenuItem>
+									<MenuItem>Editar perfil</MenuItem>
+									<MenuItem>Sair</MenuItem>
+								</MenuList>
+							</ClickAwayListener>
+						</Paper>
+					) : (
+						""
+					)}
 				</div>
 			</div>
 		</div>
