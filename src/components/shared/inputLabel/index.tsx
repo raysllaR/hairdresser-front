023d8d1797a9455ace43d727b2f04f51d395bf82
Rayslla Rodrigues <rayslla.rodrigues.ferreira@gmail.com>
2023-06-04 00:56:20 -0300

    feat: ajuste do input inputlabel

--- src/components/shared/inputLabel/index.tsx
+++ src/components/shared/inputLabel/index.tsx
@@ -2,15 +2,21 @@ import { InputBase, InputLabel } from "@mui/material";
 import React from "react";
 import "./style.css";
 
-type TType = "email" | "password" | "text";
+type TType = "email" | "password" | "text" | "number";
 
 type TInput = {
 	label: string;
 	type?: TType;
 	requered?: boolean;
+	placeholder?: string;
 };
 
-const Input: React.FC<TInput> = ({ label, type = "text", requered = true }) => {
+const Input: React.FC<TInput> = ({
+	label,
+	type = "text",
+	requered = true,
+	placeholder = "",
+}) => {
 	return (
 		<div id="container-input">
 			<InputLabel
@@ -20,6 +26,7 @@ const Input: React.FC<TInput> = ({ label, type = "text", requered = true }) => {
 			</InputLabel>
 			<InputBase
 				id="my-input"
+				placeholder={placeholder}
 				className="input-padrao"
 				type={type}
 				required={requered}
