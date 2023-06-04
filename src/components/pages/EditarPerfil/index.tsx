0cfda528a53d6e42b51fc71950c6f5e8d94078dc
Rayslla Rodrigues <rayslla.rodrigues.ferreira@gmail.com>
2023-06-04 00:24:59 -0300

    feat: ajuste na page EditarPerfil  - passando o componente ImageUpdoad e add os buttons

--- src/components/pages/EditarPerfil/index.tsx
+++ src/components/pages/EditarPerfil/index.tsx
@@ -2,29 +2,50 @@ import Header from "../../shared/Header";
 import React from "react";
 import "./style.css";
 import InputLabel from "../../shared/inputLabel";
+import ImageUpload from "../../shared/ImageUpload";
+import { ButtonBase } from "@mui/material";
 
 const EditarPerfil: React.FC = () => {
 	return (
 		<div
 			id="editar-perfil"
 			className="page">
-			<Header />
-			<form action="">
-				<InputLabel label="Nome" />
-				<InputLabel label="E-mail" />
-				<InputLabel
-					label="Senha atual"
-					type="password"
-				/>
-				<InputLabel
-					label="Nova atual"
-					type="password"
-				/>
-				<InputLabel
-					label="Confirmar senha"
-					type="password"
-				/>
-			</form>
+			<div className="body">
+				<Header />
+				<h2 className="title">Editar Perfil</h2>
+				<ImageUpload />
+				<form action="">
+					<InputLabel label="Nome" />
+					<InputLabel
+						label="Email"
+						type="email"
+					/>
+					<InputLabel
+						label="Senha atual"
+						type="password"
+					/>
+					<InputLabel
+						label="Nova atual"
+						type="password"
+					/>
+					<InputLabel
+						label="Confirmar senha"
+						type="password"
+					/>
+					<div className="container-buttons">
+						<ButtonBase
+							className="button"
+							type="submit">
+							Cancelar
+						</ButtonBase>
+						<ButtonBase
+							className="button"
+							type="submit">
+							Editar
+						</ButtonBase>
+					</div>
+				</form>
+			</div>
 		</div>
 	);
 };
