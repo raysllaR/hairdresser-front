49a7b859e11cd808fb9dc541edd00b029e7741a6
Rayslla Rodrigues <rayslla.rodrigues.ferreira@gmail.com>
2023-06-04 15:36:30 -0300

    feat: alterando para o DateCalendar o component utilizado do materil ui

--- src/components/shared/Calendar/index.tsx
+++ src/components/shared/Calendar/index.tsx
@@ -1,12 +1,20 @@
 import * as React from "react";
+import dayjs, { Dayjs } from "dayjs";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { StaticDatePicker } from "@mui/x-date-pickers/StaticDatePicker";
-
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import "./style.css";
 const Calendar = () => {
+	const [value, setValue] = React.useState<Dayjs | null>(dayjs(new Date()));
+
 	return (
 		<LocalizationProvider dateAdapter={AdapterDayjs}>
-			<StaticDatePicker />
+			<DateCalendar
+				className="calendar"
+				value={value}
+				onChange={(newValue) => setValue(newValue)}
+				dayOfWeekFormatter={(day) => `${day}.`}
+			/>
 		</LocalizationProvider>
 	);
 };
