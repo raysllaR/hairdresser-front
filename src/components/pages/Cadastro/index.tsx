55ca15343820aa510c2406adc64ad5d66fbac7db
Rayslla Rodrigues <rayslla.rodrigues.ferreira@gmail.com>
2023-06-05 01:12:09 -0300

    feat: add navigation no component de cadastro

--- src/components/pages/Cadastro/index.tsx
+++ src/components/pages/Cadastro/index.tsx
@@ -1,26 +1,34 @@
 import React from "react";
-import "./style.css";
-import Logo from "../../shared/Logo";
 import CardComponent from "../../shared/CardComponent";
 import FormCadastroRapido from "../../shared/formCadastroRapido";
+import imgLogo from "../../../assets/images/logos/logo.svg";
+import { Link } from "react-router-dom";
+import "./style.css";
 
 const Cadastro: React.FC = () => {
 	return (
-		<div className="cadastro">
+		<div
+			className="page"
+			id="cadastro">
 			<div className="container-cadastro">
-				<div className="cadastro-navigator">
-					<a href="">Home</a> {">"} Área de cadastro
+				<div className="navigator">
+					<Link to={"/login"}>Home</Link> {">"} Área de cadastro
 				</div>
-				<Logo />
-				<CardComponent>
-					<span className="title">Área de Cadastro</span>
-					<FormCadastroRapido />
-					<div className="more">
-						<span>
-							Já tem cadastro? <a href="">Voltar à Página Inicial</a>
-						</span>
-					</div>
-				</CardComponent>
+				<div className="container">
+					<img
+						src={imgLogo}
+						alt="Logo"
+					/>
+					<CardComponent>
+						<h1>Área de Cadastro</h1>
+						<FormCadastroRapido />
+						<div className="links">
+							<span>
+								Já tem cadastro? <a href="">Voltar à Página Inicial</a>
+							</span>
+						</div>
+					</CardComponent>
+				</div>
 			</div>
 		</div>
 	);
