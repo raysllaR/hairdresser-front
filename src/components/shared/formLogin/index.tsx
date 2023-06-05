c2d7b41303dd7ee28c89eb88aa72f5e82d9dd502
Rayslla Rodrigues <rayslla.rodrigues.ferreira@gmail.com>
2023-06-04 21:29:55 -0300

    chore: comentando o component Logo

--- src/components/shared/formLogin/index.tsx
+++ src/components/shared/formLogin/index.tsx
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ButtonBase } from "@mui/material";
 import iconMail from "../../../assets/images/icons/mail.svg";
-
-import "./style.css";
 import InputIcon from "../inputIcon";
 import InputIconPassword from "../inputIconPassword";
+import "./style.css";
+import { useNavigate } from "react-router-dom";
 
 const FormLogin: React.FC = () => {
+	const navigate = useNavigate();
+	const logar = (): void => {
+		localStorage.setItem("login", "logado");
+		navigate("/");
+	};
+
 	return (
-		<form action="">
-			<InputIcon
-				type="email"
-				placeholder="Email"
-				startAdornmentImgPath={iconMail}
-			/>
-			<InputIconPassword />
+		<form
+			id="form-login"
+			action=""
+			onSubmit={logar}>
+			<div className="container-form-login">
+				<InputIcon
+					type="email"
+					placeholder="Email"
+					startAdornmentImgPath={iconMail}
+				/>
+				<InputIconPassword />
+			</div>
 
 			<ButtonBase
 				className="button"
