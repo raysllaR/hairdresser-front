0f03d398d3f44329bac76c762c448f56e18e5e69
Rayslla Rodrigues <rayslla.rodrigues.ferreira@gmail.com>
2023-06-04 21:44:34 -0300

    feat: verificando se o usuário já estava logado e navegando entre as pages

--- src/components/pages/Login/index.tsx
+++ src/components/pages/Login/index.tsx
@@ -1,23 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import FormLogin from "../../shared/formLogin";
 import CardComponent from "../../shared/CardComponent";
+import imgLogo from "../../../assets/images/logos/logo.svg";
 import "./index.css";
-import Logo from "../../shared/Logo";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login: React.FC = () => {
+	const navigate = useNavigate();
+
+	useEffect((): void => {
+		const isLogin: boolean = localStorage.getItem("login") !== null;
+
+		if (isLogin) {
+			navigate("/");
+		}
+	}, []);
+
 	return (
-		<div id="app">
+		<div
+			id="login"
+			className="page">
 			<div className="container">
-				<Logo />
+				<img
+					src={imgLogo}
+					alt="Logo"
+				/>
 				<CardComponent>
-					<span className="title">Olá! Seja Bem-Vindo!</span>
+					<h1 className="title">Olá! Seja Bem-Vindo!</h1>
 					<FormLogin />
-					<div className="more">
+					<div className="links">
 						<span>
-							Esqueceu sua senha? <a href="">Recuperar</a>
+							Esqueceu sua senha? <Link to="/">Recuperar</Link>
 						</span>
 						<span>
-							Ainda não tem conta? <a href="">Cadastre-se</a>
+							Ainda não tem conta? <Link to="/cadastro">Cadastre-se</Link>
 						</span>
 					</div>
 				</CardComponent>
