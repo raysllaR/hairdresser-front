b49792c0d4eee974a91950cf4177bf9897221da2
Rayslla Rodrigues <rayslla.rodrigues.ferreira@gmail.com>
2023-06-05 01:15:12 -0300

    feat: add navigator para o component CadastroRapido

--- src/components/shared/formCadastroRapido/index.tsx
+++ src/components/shared/formCadastroRapido/index.tsx
@@ -4,19 +4,31 @@ import InputIconPassword from "../inputIconPassword";
 import React from "react";
 import iconPerson from "../../../assets/images/icons/person.svg";
 import iconMail from "../../../assets/images/icons/mail.svg";
+import "./style.css";
+import { useNavigate } from "react-router-dom";
 
 const FormCadastroRapido: React.FC = () => {
+	const navigate = useNavigate();
+	const cadastrar = (): void => {
+		navigate("/login");
+	};
+
 	return (
-		<form action="">
-			<InputIcon
-				placeholder="Nome"
-				startAdornmentImgPath={iconPerson}
-			/>
-			<InputIcon
-				placeholder="Email"
-				startAdornmentImgPath={iconMail}
-			/>
-			<InputIconPassword />
+		<form
+			id="form-cadastro-rapido"
+			action=""
+			onSubmit={cadastrar}>
+			<div className="container-inputs">
+				<InputIcon
+					placeholder="Nome"
+					startAdornmentImgPath={iconPerson}
+				/>
+				<InputIcon
+					placeholder="Email"
+					startAdornmentImgPath={iconMail}
+				/>
+				<InputIconPassword />
+			</div>
 
 			<ButtonBase
 				className="button"
