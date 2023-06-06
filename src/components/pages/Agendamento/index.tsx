88e964e9b6a37d9cb8040a17d2c385e04a41d529
Rayslla Rodrigues <rayslla.rodrigues.ferreira@gmail.com>
2023-06-06 10:47:00 -0300

    feat: ajustando e add os inputs de date e time

--- src/components/pages/Agendamento/index.tsx
+++ src/components/pages/Agendamento/index.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import "./style.css";
 import InputLabel from "../../shared/inputLabel";
 import ButtonComponent from "../../shared/ButtonComponent";
-import DatePickerComponent from "../../shared/DatePickerComponent";
 
 const Agendamento: React.FC = () => {
 	return (
@@ -11,36 +10,45 @@ const Agendamento: React.FC = () => {
 			id="agendamento"
 			className="page">
 			<Header />
-			<h2 className="title">Agendamento de Horário</h2>
-			<div className="body">
-				<form
-					action=""
-					style={{ display: "flex", flexDirection: "column", gap: 8 }}>
-					<InputLabel label="Nome do Cliente" />
-					<InputLabel
-						label="Celular"
-						placeholder="(xx) xxxxx-xxxx"
-						type="text"
-					/>
-					<InputLabel
-						label="Date"
-						placeholder="MM/DD/AAAA"
-						// type="date"
-					/>
-					{/* <DatePickerComponent /> */}
+			<div className="container">
+				<h2 className="title">Agendamento de Horário</h2>
+				<div className="body">
+					<form
+						action=""
+						style={{ display: "flex", flexDirection: "column", gap: 8 }}>
+						<InputLabel label="Nome do Cliente" />
+						<InputLabel
+							label="Celular"
+							placeholder="(xx) xxxxx-xxxx"
+							type="text"
+						/>
+						<div className="container-date-time">
+							<InputLabel
+								label="Data"
+								type="date"
+								width={130}
+							/>
+							<InputLabel
+								label="Horário"
+								type="time"
+								width={130}
+							/>
+						</div>
+						{/* <DatePickerComponent /> */}
 
-					<div className="container-buttons">
-						<ButtonComponent
-							type="reset"
-							text="Cancelar"
-							className="reverse"
-						/>
-						<ButtonComponent
-							type="submit"
-							text="Salvar"
-						/>
-					</div>
-				</form>
+						<div className="container-buttons">
+							<ButtonComponent
+								type="reset"
+								text="Cancelar"
+								className="reverse"
+							/>
+							<ButtonComponent
+								type="submit"
+								text="Salvar"
+							/>
+						</div>
+					</form>
+				</div>
 			</div>
 		</div>
 	);
