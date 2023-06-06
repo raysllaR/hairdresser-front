a5ea60511b2fb723df1cb3b13ea3c2bfcaa5b721
Rayslla Rodrigues <rayslla.rodrigues.ferreira@gmail.com>
2023-06-06 10:28:53 -0300

    feat: ajustes no locale e nas configurações passadas para o calendar

--- src/components/shared/Calendar/index.tsx
+++ src/components/shared/Calendar/index.tsx
@@ -3,17 +3,30 @@ import dayjs, { Dayjs } from "dayjs";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+
+import "dayjs/locale/pt-br";
 import "./style.css";
+
 const Calendar = () => {
 	const [value, setValue] = React.useState<Dayjs | null>(dayjs(new Date()));
 
+	const formatDayOfWeek = (day: string) => {
+		const weekdays = ["Dom.", "Seg.", "Ter.", "Quar.", "Qui.", "Sex.", "Sáb."];
+		return weekdays[parseInt(day, 10)];
+	};
+
 	return (
-		<LocalizationProvider dateAdapter={AdapterDayjs}>
+		<LocalizationProvider
+			dateAdapter={AdapterDayjs}
+			adapterLocale="pt-BR">
 			<DateCalendar
 				className="calendar"
 				value={value}
 				onChange={(newValue) => setValue(newValue)}
-				dayOfWeekFormatter={(day) => `${day}.`}
+				dayOfWeekFormatter={formatDayOfWeek}
+				disablePast={true}
+				openTo="day"
+				autoFocus={true}
 			/>
 		</LocalizationProvider>
 	);
