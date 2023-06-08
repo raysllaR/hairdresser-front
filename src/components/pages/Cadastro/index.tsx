49cca1035ba6a8a194181df202068a3015e8b3c1
Rayslla Rodrigues <rayslla.rodrigues.ferreira@gmail.com>
2023-06-07 22:57:55 -0300

    refactor: add o PageContainer na page de Cadastro

--- src/components/pages/Cadastro/index.tsx
+++ src/components/pages/Cadastro/index.tsx
@@ -4,33 +4,32 @@ import FormCadastroRapido from "../../shared/formCadastroRapido";
 import imgLogo from "../../../assets/images/logos/logo.svg";
 import { Link } from "react-router-dom";
 import "./style.css";
+import PageContainer from "../../shared/PageContainer";
 
 const Cadastro: React.FC = () => {
 	return (
-		<div
-			className="page"
-			id="cadastro">
-			<div className="container-cadastro">
-				<div className="navigator">
-					<Link to={"/login"}>Home</Link> {">"} Área de cadastro
-				</div>
-				<div className="container">
-					<img
-						src={imgLogo}
-						alt="Logo"
-					/>
-					<CardComponent>
-						<h1>Área de Cadastro</h1>
-						<FormCadastroRapido />
-						<div className="links">
-							<span>
-								Já tem cadastro? <a href="">Voltar à Página Inicial</a>
-							</span>
-						</div>
-					</CardComponent>
-				</div>
+		<PageContainer
+			idPage="cadastro"
+			classNameContainer="container">
+			<p className="navigator">
+				<Link to={"/login"}>Home</Link> {">"} Área de cadastro
+			</p>
+			<div className="body">
+				<img
+					src={imgLogo}
+					alt="Logo"
+				/>
+				<CardComponent>
+					<h1>Área de Cadastro</h1>
+					<FormCadastroRapido />
+					<div className="links">
+						<span>
+							Já tem cadastro? <a href="">Voltar à Página Inicial</a>
+						</span>
+					</div>
+				</CardComponent>
 			</div>
-		</div>
+		</PageContainer>
 	);
 };
 
